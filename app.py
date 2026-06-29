d8b1e54bf4b07b62338fb6eedfde249c422bd67d
ntna <ntngft@gmail.com>
2026-06-29 23:13:37 +0700

    add port and template

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['username']
+        return f"Hello {name}, POST request received"
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
